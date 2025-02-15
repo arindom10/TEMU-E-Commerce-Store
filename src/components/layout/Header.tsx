bf68d3f47a7ad5d2757bf4b6a2dc055871ed1f5d
arindom10 <karindom7@gmail.com>
2025-02-15 11:47:58 +0600

    refactor layout and add Header component with scroll behavior

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const AnnouncmentBar = () => {
+  return (
+    <div className="w-full bg-black py-2">
+      <div className="container mx-auto flex items-center justify-center px-8">
+        <span className="text-center text-sm font-medium tracking-wide text-white">
+          FREE SHIPPING ON ORDERS OVER $15.00 X FREE RETURNS
+        </span>
+      </div>
+    </div>
+  );
+};
+const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [prevScrollY, setPrevScrollY] = useState<number>(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      const scrolledUp = currentScrollY < prevScrollY;
+
+      if (scrolledUp) {
+        setIsOpen(true);
+      } else if (currentScrollY > 100) {
+        setIsOpen(false);
+      }
+      setPrevScrollY(currentScrollY);
+    };
+
+    setPrevScrollY(window.scrollY);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className="w-full sticky top-0 z-50">
+      <div className="w-full transform transition-transform duration-300 ease-in-out">
+        <AnnouncmentBar />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
